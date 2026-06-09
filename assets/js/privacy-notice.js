0bab89bca41a7bc93af8e20a74963274d0b253e0
Markides.Markellos <markellos.markides@gmail.com>
2026-06-09 13:58:39 +0300

    Persist privacy notice acceptance in local storage

--- assets/js/privacy-notice.js
+++ assets/js/privacy-notice.js
@@ -1,5 +1,23 @@
+const privacyNoticeStorageKey = "organizeYourPcPrivacyNoticeAccepted";
+
 function getCookieNoticeElement() {
   return document.getElementById("cookieNotice");
+}
+
+function hasAcceptedPrivacyNotice() {
+  try {
+    return localStorage.getItem(privacyNoticeStorageKey) === "true";
+  } catch (error) {
+    return false;
+  }
+}
+
+function rememberPrivacyNoticeAccepted() {
+  try {
+    localStorage.setItem(privacyNoticeStorageKey, "true");
+  } catch (error) {
+    // If local storage is unavailable, the notice can still be hidden for the current page view.
+  }
 }
 
 function showCookieNotice() {
@@ -20,19 +38,28 @@ function hideCookieNotice() {
   notice.hidden = true;
 }
 
+function acceptCookieNotice() {
+  rememberPrivacyNoticeAccepted();
+  hideCookieNotice();
+}
+
 function initialiseCookieNoticeControls() {
   const acceptButton = document.getElementById("cookieAcceptButton");
   const laterButton = document.getElementById("cookieLaterButton");
 
   if (acceptButton) {
-    acceptButton.addEventListener("click", hideCookieNotice);
+    acceptButton.addEventListener("click", acceptCookieNotice);
   }
 
   if (laterButton) {
     laterButton.addEventListener("click", hideCookieNotice);
   }
 
-  showCookieNotice();
+  if (hasAcceptedPrivacyNotice()) {
+    hideCookieNotice();
+  } else {
+    showCookieNotice();
+  }
 }
 
 window.hideCookieNotice = hideCookieNotice;
