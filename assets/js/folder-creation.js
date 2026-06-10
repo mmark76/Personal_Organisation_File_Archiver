527c8f50fefb9d2d54627298dfbc1f5c1782dc37
Markides.Markellos <markellos.markides@gmail.com>
2026-06-10 13:19:45 +0300

    Update generated root folder name

--- assets/js/folder-creation.js
+++ assets/js/folder-creation.js
@@ -1,7 +1,7 @@
 /* User-controlled folder tree creation on the local computer. */
 
 window.FolderCreation = (() => {
-  const appRootFolderName = "Organize Your PC";
+  const appRootFolderName = "Personal Memory-Based File Archiver";
 
   async function createDirectoryPath(rootHandle, folderPath) {
     const parts = folderPath.split("/").filter(Boolean);
